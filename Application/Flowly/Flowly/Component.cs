46ce892771433b071f0dd5c32ad7eadbda69370a
NikolaNikushev <nikola.nikushev@hotmail.com>
2015-12-02 17:58:39 +0100

    submit of class diagram

---
+++ Application/Flowly/Flowly/Component.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flowly
+{
+    class Component:IDeletable,IEditable
+    {
+        Boolean IDeletable.Delete()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        Boolean IEditable.Edit()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
